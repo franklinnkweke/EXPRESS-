8bbdad0ad62cdb3d5347e9f53ba0736f923e2b9a
Franklin Nkweke <franklinnkweke@gmail.com>
2023-03-02 10:55:50 +0100

    done

---
+++ dist/routes/index.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const userController_1 = require("../controller/userController");
+const router = express_1.default.Router();
+/* GET home page. */
+router.get('/register', (req, res) => {
+    res.render("register");
+});
+router.post('/register', userController_1.RegisterUser);
+router.get('/dashboard', userController_1.getUniqueUsersMovies);
+router.get('/unique', userController_1.getUsers);
+router.get('/login', (req, res) => {
+    res.render("login");
+});
+router.post('/login', userController_1.LoginUser);
+router.get('/logout', (req, res) => {
+    try {
+        res.clearCookie("token");
+        res.clearCookie("id");
+        res.redirect('/movies');
+    }
+    catch (err) {
+        res.status(400).json({ msg: "error" });
+    }
+});
+exports.default = router;
---
+++ src/routes/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import { LoginUser, RegisterUser,getUniqueUsersMovies,getUsers } from '../controller/userController';
+
+const router = express.Router();
+
+
+/* GET home page. */
+router.get('/register',(req,res)=>{
+  res.render("register")
+} )
+
+router.post('/register',RegisterUser)
+router.get('/dashboard',getUniqueUsersMovies)
+router.get('/unique',getUsers)
+
+router.get('/login',(req,res)=>{
+  res.render("login")
+})
+router.post('/login',LoginUser)
+router.get('/logout', (req,res)=>{
+   
+  try{
+    res.clearCookie("token")
+    res.clearCookie("id")  
+    res.redirect('/movies')
+  }catch(err){
+    res.status(400).json({msg:"error"})
+  }
+})
+
+
+
+export default router;
